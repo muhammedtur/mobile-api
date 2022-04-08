eff8adb30fa8f5b725dec10cda77feb21890fe36
muhammedtur <muhammedabduh.tur@arcelik.com>
2022-04-08 23:02:08 +0300

    default language value changed

--- database/migrations/2022_04_07_000558_create_device_table.php
+++ database/migrations/2022_04_07_000558_create_device_table.php
@@ -22,11 +22,12 @@ class CreateDeviceTable extends Migration
             $table->string('name', 50)->nullable();
             $table->string('appId');
             $table->string('clientToken');
-            $table->string('language');
+            // Default value set to en. Could be change
+            $table->string('language')->default('en');
             // Field type could be integer depending by os type. Ex. 0: Android, 1: IOS etc.
             $table->string('os');
             $table->string('subscription')->default('started');
-            // New register. Subscription due date 1 month from now. Could be changed
+            // New register. Subscription due date 1 month from now. Could be change
             $table->datetime('subscription_expire_date')->default(Carbon::now()->format('Y-m-d H:i:s')->addMonth());
             $table->timestamps();
         });
